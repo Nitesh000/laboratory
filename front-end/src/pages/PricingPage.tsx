f0807b2e75e96329081041253463944513cf0b32
Nitesh000 <ntudu040@gmail.com>
2023-01-23 20:41:50 +0530

    feat(front-end(pricingpage)): data on the html page
    done with the fetching and organizing data, But have to make it look good. and the footer is stuck
    on the middle of the page.
    
    BREAKING CHANGE: footer is stuck in the middle
    
    fix 728666b11f98df5f64ef0f4ec002d4099e79abf0 problem of having infinite loop

--- front-end/src/pages/PricingPage.tsx
+++ front-end/src/pages/PricingPage.tsx
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 
+type TestsTypes = {
+  _id: string;
+  test: string;
+  price: number;
+};
+
+type PricingType = {
+  _id: string;
+  name: string;
+  tests: [TestsTypes];
+};
+
 const PricingPagae = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<PricingType[]>([]);
   useEffect(() => {
     const dataFetch = async () => {
       const data = await fetch("http://localhost:3000/pricing");
@@ -11,19 +23,50 @@ const PricingPagae = () => {
     };
     dataFetch();
     return () => {
-      setData(null);
+      setData([]);
     };
   }, []);
   console.log(data);
   return (
     <React.Fragment>
       <Header location="Pricing" />
-      <section className="max-w-7xl h-screen">
+      <section className="max-w-7xl h-screen mb-2">
         <div className="flex justify-center items-center h-28">
           <h3 className="tracking-wider text-darkGreen text-2xl hover:underline hover:decoration-darkGreen hover:cursor-pointer">
             Pricing
           </h3>
         </div>
+        {data.map((item: PricingType) => {
+          return (
+            <div key={item._id}>
+              <h1 className="tracking-wider text-darkGreen text-xl hover:underline hover:decoration-darkGreen hover:cursor-pointer">
+                {item.name}
+              </h1>
+              <table className="w-1/2 inline text-sm text-left text-gray-500 ">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      Name of the test
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Price
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {item.tests.map((test: TestsTypes, i) => {
+                    return (
+                      <tr key={i} className="bg-white border-b ">
+                        <td className="px-6 py-4">{test.test}</td>
+                        <td className="px-6 py-4">{test.price}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
       </section>
     </React.Fragment>
   );
